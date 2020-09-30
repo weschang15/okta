5ad51e433ff767ca8c2d7002445c5857f64f7175
Wesley Chang <weschang15@gmail.com>
2020-09-30 09:45:27 -0700

    feat: add intro and links to task pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,31 @@
-import React from "react"
 import { Link } from "gatsby"
-
+import React from "react"
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Section from "../components/section/Section"
+import { SectionContent } from "../components/section/SectionContent"
+import { SectionTitle } from "../components/section/SectionTitle"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Section>
+      <SectionTitle>Welcome!</SectionTitle>
+      <SectionContent>
+        <p>Use the links below to navigate between pages.</p>
+        <p>
+          Not everything on my wishlist was completed but I was able to complete
+          all tasks.
+        </p>
+        <p>
+          If I had the time, I would've paginated the posts archive and
+          incorporated a primary navigation.
+        </p>
+        <Link to="/posts">Go to Posts archive</Link> <br />
+        <Link to="/sponsors">Go to Sponsors page</Link> <br />
+        <Link to="/contact">Go to Contact page</Link>
+      </SectionContent>
+    </Section>
   </Layout>
 )
 
